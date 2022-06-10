bc3396373cb8baa66d7428ae5ac143f92b1e2c01
Thapelo <dev.mothapotr@gmail.com>
2022-06-10 15:14:27 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,82 @@
-import * as React from "react"
 import {
-  ChakraProvider,
   Box,
-  Text,
+  Button,
+  ChakraProvider,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
   Link,
-  VStack,
-  Code,
-  Grid,
+  SimpleGrid,
+  Stack,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  useColorModeValue,
+} from "@chakra-ui/react";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = () => {
+  return (
+    <ChakraProvider theme={theme}>
+      <SimpleGrid columns={2} spacing={10}>
+        <Flex
+          minH={"100vh"}
+          align={"center"}
+          justify={""}
+          bg={useColorModeValue("gray.50", "gray.800")}
+        >
+          <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+            <Stack align={"center"}>
+              <Heading fontSize={"3xl"}>Welcome Back</Heading>
+            </Stack>
+            <Box
+              rounded={"lg"}
+              bg={useColorModeValue("white", "gray.700")}
+              boxShadow={"lg"}
+              p={8}
+              marginTop={5}
+            >
+              <Stack spacing={4}>
+                <FormControl id="email">
+                  <FormLabel>Email address</FormLabel>
+                  <Input type="email" />
+                </FormControl>
+                <FormControl id="password">
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" />
+                </FormControl>
+                <Stack spacing={10}>
+                  <Stack
+                    direction={{ base: "column", sm: "row" }}
+                    align={"start"}
+                    justify={"space-between"}
+                  >
+                    <Checkbox>Remember for 30 days</Checkbox>
+                    <Link color={"blue.400"}>Forgot password?</Link>
+                  </Stack>
+                  <Button
+                    bg={"#7F56D9"}
+                    color={"white"}
+                    _hover={{
+                      bg: "blue.500",
+                    }}
+                  >
+                    Sign in
+                  </Button>
+                </Stack>
+              </Stack>
+            </Box>
+          </Stack>
+        </Flex>
+        <Flex
+          minH={"100vh"}
+          align={"center"}
+          justify={""}
+          bg={useColorModeValue("gray.50", "gray.800")}
+        >
+          Image here
+        </Flex>
+      </SimpleGrid>
+    </ChakraProvider>
+  );
+};
